70a09c2204125665ffe8ee30ab4f1bd18254f42f
Anastasia Belaya <anastasiy.litvin@gmail.com>
2019-07-03 19:14:49 +0300

    project

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    var swiper = new Swiper('.swiper-container', {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+
+});
